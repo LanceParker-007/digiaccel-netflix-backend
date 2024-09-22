73d28f375a1e41a13746641bba53a87c961aaafb
lance <lanceparker2001@gmail.com>
2024-09-23 03:38:44 +0530

    Trying to fix 504

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  if (mongoose.connections[0].readyState) {
+    // Use current connection if already connected
+    return;
+  }
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
-    process.exit(1); // Exit with failure
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with failure
   }
 };
 
