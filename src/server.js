8abbebb1733cc6b2c20f389086c4d4a7608deb22
lance <lanceparker2001@gmail.com>
2024-09-23 03:09:44 +0530

    Why 504 coming? Issue found, mongodb not connecting in deployment

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ import connectDB from "./config/db.js";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
-  await connectDB(); // Establish MongoDB connection
-  console.log(`Server running on port ${PORT}`);
+  try {
+    await connectDB(); // Establish MongoDB connection
+    console.log(`Server running on port ${PORT}`);
+  } catch (error) {
+    console.log("Some error occured!");
+  }
 });
